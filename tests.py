5b989dacb46d30c4fdcf9e121dc04297f1fac544
Lekerest <ikhritankov@yandex.ru>
2025-05-30 16:14:50 +0700

    4

--- tests.py
+++ tests.py
@@ -1,24 +1,35 @@
 from main import BooksCollector
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+########################## 1. Тест добавление 2-ух книг add_new_book ##########################
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.books_genre) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+########################## 2. Тест добавление жанра книге set_book_genre ##########################
+    def test_set_book_genre_add_book(self):
+        collector = BooksCollector()
+        collector.books_genre['Гордость и предубеждение и зомби'] = ''
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        assert (collector.books_genre['Гордость и предубеждение и зомби'] == 'Ужасы')
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+########################## 3. Тест получение жанра книги get.book.genre ##########################
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.books_genre['Гордость и предубеждение и зомби'] = 'Ужасы'
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
+
+########################## 4. Тест получение книг по жанру get_books_with_specific_genre ##########################
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.books_genre['Гордость и предубеждение и зомби'] = 'Ужасы'
+        collector.books_genre['Астрал'] = 'Ужасы'
+        collector.books_genre['Папочки'] = 'Комедии'
+        collector.books_genre['Мамочки'] = 'Детективы'
+        assert len(collector.get_books_with_specific_genre('Ужасы')) == 2
+
+
